3b924369f1e2516a9d690b3f2505aa96fdc5fccc
Vincent Kendrick <dagreatcode@gmail.com>
2022-06-05 18:20:55 -0400

    deploying

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Barbara Kendrick' Website
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
