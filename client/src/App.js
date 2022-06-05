e6cbee83aa6a76b35eab471bdc6e4751362771de
Vincent Kendrick <dagreatcode@gmail.com>
2022-06-05 18:55:42 -0400

    deploying4

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Barbara Kendrick' Website.
+          Barbara Kendrick's Website.
         </p>
       </header>
     </div>
