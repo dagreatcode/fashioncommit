5cacc4eb7f8f6f75ea4755e96171ee5c96ca4ff0
Vincent Kendrick <dagreatcode@gmail.com>
2022-09-01 16:35:51 -0400

    added fake data to redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import Posts from "./components/Posts/Posts";
-import axios from "axios";
+// import axios from "axios";
+import { useSelector } from "react-redux";
 
 function App() {
-  const [posts,setPosts] = useState([]);
-  const [myPosts,setMyPosts] = useState([]);
+  // const [posts,setPosts] = useState([]);
+  // const [myPosts,setMyPosts] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        console.log(response.data);
-        setPosts(response.data)
-        const filteredPosts = response.data.filter(post => post.userId === 1)
-        setMyPosts(filteredPosts)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  const posts = useSelector((state) => state.articles);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts")
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       setPosts(response.data)
+  //       const filteredPosts = response.data.filter(post => post.userId === 1)
+  //       setMyPosts(filteredPosts)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   return (
     <div className="container">
       <div className="row">
-        <Posts posts={posts} title="All Posts"/>
-        <Posts posts={myPosts} title="My Posts"/>
+        <Posts posts={posts} title="All Posts" />
+        {/* <Posts posts={myPosts} title="My Posts" /> */}
       </div>
       <h1>Hello World</h1>
     </div>
--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,9 +1,9 @@
-import {configureStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../redux/index";
+import {legacy_createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENION_COMPOSE__ || compose;
 
-const store = configureStore(rootReducer, storeEnhancers(applyMiddleware(thunk)));
+const store = legacy_createStore(rootReducer, storeEnhancers(applyMiddleware(thunk)));
 
 export default store;
