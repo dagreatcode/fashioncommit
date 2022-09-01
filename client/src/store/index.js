73ddeffdff9794cda3758aeb473631527f4447e3
Vincent Kendrick <dagreatcode@gmail.com>
2022-09-01 15:54:06 -0400

    added redux and thunk

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,2 +1,9 @@
-import {createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../redux/reducers";
+import {configureStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../redux/index";
+import thunk from "redux-thunk";
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENION_COMPOSE__ || compose;
+
+const store = configureStore(rootReducer, storeEnhancers(applyMiddleware(thunk)));
+
+export default store;
