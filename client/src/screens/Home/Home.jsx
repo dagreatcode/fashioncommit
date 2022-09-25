4b5c4da6ed258bbab0172f9ce5b820a4d36a8bbd
Vincent Kendrick <dagreatcode@gmail.com>
2022-09-25 10:52:50 -0400

    added the info to home page

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Posts from "../../components/Posts/Posts"
+import { useSelector } from "react-redux";
 
 function Home() {
+  const posts = useSelector((state) => state.articles);
+  const myPosts = useSelector((state) => state.myArticles);
   return (
-    <div>Home</div>
-  )
+    <div className="container">
+      <div className="row">
+        <Posts posts={posts} title="All Posts" />
+        <Posts posts={myPosts} title="My Posts" />
+      </div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
