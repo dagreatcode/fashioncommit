e1eb9095919cef2208e57a83866c810872b21dd0
Vincent Kendrick <dagreatcode@gmail.com>
2022-09-25 10:03:33 -0400

    added some main pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React, { useEffect } from "react";
-import Posts from "./components/Posts/Posts";
+// import Posts from "./components/Posts/Posts";
 // import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { getArticles, getArticlesById } from "./redux/actions/index";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Home from "./screens/Home/Home"
+import Login from "./screens/Login/Login"
+import NotFound from "./screens/NotFound/NotFound"
 
 function App() {
   // const [posts,setPosts] = useState([]);
   // const [myPosts,setMyPosts] = useState([]);
 
-  const posts = useSelector((state) => state.articles);
-  const myPosts = useSelector((state) => state.myArticles);
+  // const posts = useSelector((state) => state.articles);
+  // const myPosts = useSelector((state) => state.myArticles);
   const error = useSelector((state) => state.error);
   const loading = useSelector((state) => state.loading);
   // const reload = useSelector((state) => state.reload);
@@ -25,12 +29,12 @@ function App() {
     // return () => {};
   }, [dispatch]);
 
-  if (error){
-    return <h1>{error}</h1>
+  if (error) {
+    return <h1>{error}</h1>;
   }
 
-  if (loading){
-    return <h1>Loading . . .</h1>
+  if (loading) {
+    return <h1>Loading . . .</h1>;
   }
   // useEffect(() => {
   //   axios
@@ -47,13 +51,23 @@ function App() {
   // }, []);
 
   return (
-    <div className="container">
-      <div className="row">
-        <Posts posts={posts} title="All Posts" />
-        <Posts posts={myPosts} title="My Posts" />
-      </div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route exact path="/Login" element={<Login/>}/>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="*" element={<NotFound/>}/>
+        {/* <Route path="" element={<Home />} /> */}
+      </Routes>
+    </Router>
+      {/* <div className="container">
+        <div className="row">
+          <Posts posts={posts} title="All Posts" />
+          <Posts posts={myPosts} title="My Posts" />
+        </div>
+        <h1>Hello World</h1>
+      </div> */}
+    </>
   );
 }
 
