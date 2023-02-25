720f493c948d02a5b2adf54f10d3fe309e7db944
Vincent Kendrick <dagreatcode@gmail.com>
2023-02-24 19:34:37 -0500

    clean up

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -29,14 +29,13 @@ function NavBar() {
             // style={{backgroundColor: "#65c9ff"}}
           >
             <span className="navbar-toggler-icon">
-              <i className="fas fa-bars"/>
+              <i className="fas fa-bars" />
             </span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item"        style={{color: "white"}}>
+              <li className="nav-item" style={{ color: "white" }}>
                 <Link
-           
                   className="nav-link active"
                   aria-current="page"
                   to="/UpLoadFile"
@@ -58,75 +57,81 @@ function NavBar() {
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button className="btn btn-outline-primary" style={{backgroundColor: "white",  color: "#3330E4" }} type="submit">
+              <button
+                className="btn btn-outline-primary"
+                style={{ backgroundColor: "white", color: "#3330E4" }}
+                type="submit"
+              >
                 Search
               </button>
             </form>
-    
-              <br />
-              <div className="dropdown">
-                <button
 
-                  style={{margin: "5px", backgroundColor:"white", color: "#3330E4"}}
-                  type="button"
-                  className="btn dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                  data-bs-auto-close="outside"
-                >
-                  Sign In
+            <br />
+            <div className="dropdown">
+              <button
+                style={{
+                  margin: "5px",
+                  backgroundColor: "white",
+                  color: "#3330E4",
+                }}
+                type="button"
+                className="btn dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                data-bs-auto-close="outside"
+              >
+                Sign In
+              </button>
+              <form className="dropdown-menu p-4">
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleDropdownFormEmail2"
+                    className="form-label"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleDropdownFormEmail2"
+                    placeholder="email@example.com"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleDropdownFormPassword2"
+                    className="form-label"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleDropdownFormPassword2"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="mb-3">
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="dropdownCheck2"
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor="dropdownCheck2"
+                    >
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Sign in
                 </button>
-                <form className="dropdown-menu p-4">
-                  <div className="mb-3">
-                    <label
-                      htmlFor="exampleDropdownFormEmail2"
-                      className="form-label"
-                    >
-                      Email address
-                    </label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="exampleDropdownFormEmail2"
-                      placeholder="email@example.com"
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label
-                      htmlFor="exampleDropdownFormPassword2"
-                      className="form-label"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="exampleDropdownFormPassword2"
-                      placeholder="Password"
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <div className="form-check">
-                      <input
-                        type="checkbox"
-                        className="form-check-input"
-                        id="dropdownCheck2"
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor="dropdownCheck2"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-                  </div>
-                  <button type="submit" className="btn btn-primary">
-                    Sign in
-                  </button>
-                </form>
-              </div>
+              </form>
             </div>
-        
+          </div>
         </div>
       </nav>
     </>
