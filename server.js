fc822493584ec8bddfe912743988d7e36f640f2c
Vincent Kendrick <dagreatcode@gmail.com>
2023-03-09 12:36:26 -0500

    last try before i use firebase for uploading photos

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 app.use(express.static("uploads"));
+
 mongoose.set("strictQuery", false);
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/fashioncommit_db",
