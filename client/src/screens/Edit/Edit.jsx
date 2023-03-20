0acadb2341c09e6f9a81e938cfc269a5cfa88c9a
Vincent Kendrick <dagreatcode@gmail.com>
2023-03-20 01:53:53 -0400

    working

--- client/src/screens/Edit/Edit.jsx
+++ client/src/screens/Edit/Edit.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 // import PropTypes from 'prop-types'
 import EModal from "../../components/UpLoadModal/Edit";
+import { useSelector } from "react-redux";
+import Posts from "../../components/Posts/EditPosts";
 
 const Edit = () => {
+  const myPosts = useSelector((state) => state.myArticles);
+
   return (
     <>
       <h1>Edit Page</h1>
       <EModal />
+      <Posts posts={myPosts} title="Time to Edit✒️" />
+
     </>
   );
 };
