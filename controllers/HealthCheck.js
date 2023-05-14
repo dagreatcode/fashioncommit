661b3748df8a69d931ab745fe9b9a258342152f6
Vincent Kendrick <dagreatcode@gmail.com>
2023-05-13 20:51:26 -0400

    cleaning up

--- controllers/HealthCheck.js
+++ controllers/HealthCheck.js
@@ -2,19 +2,18 @@
 const express = require("express");
 const router = express.Router({});
 
-router.get('/', async (_req, res, _next) => {
-
-    const healthCheck = {
-        uptime: process.uptime(),
-        message: 'OK',
-        timestamp: Date.now()
-    };
-    try {
-        res.send(healthCheck);
-    } catch (error) {
-        healthCheck.message = error;
-        res.status(503).send();
-    }
+router.get("/", async (_req, res, _next) => {
+  const healthCheck = {
+    uptime: process.uptime(),
+    message: "OK",
+    timestamp: Date.now(),
+  };
+  try {
+    res.send(healthCheck);
+  } catch (error) {
+    healthCheck.message = error;
+    res.status(503).send();
+  }
 });
 // export router with all routes included
 module.exports = router;
